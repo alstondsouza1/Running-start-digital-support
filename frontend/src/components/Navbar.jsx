2b1c9df456a41d8bd2b372f770f53b43f079cad2
qopci <seonpaia@gmail.com>
2026-01-30 12:17:14 -0800

    Added a favicon of GRC Logo, added a Logo for the Navbar, modified the HTML page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,18 @@
 import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 import { Link } from "react-router-dom";
+import Logo from "../assets/GRC_Logo_WHITE.png";
 
 export default function Navbar() {
   return (
     <AppBar position="fixed" sx={{ backgroundColor: "green" }}>
       <Toolbar>
+        <Link to="/">
+          <img src={Logo} alt="Logo" style={{ height: 50, marginRight: 16, transition: "filter 0.3s" }} 
+          onMouseEnter={e => (e.currentTarget.style.filter = "brightness(0) saturate(100%) invert(31%) sepia(84%) saturate(5931%) hue-rotate(3deg) brightness(95%) contrast(100%)")}
+          onMouseLeave={e => (e.currentTarget.style.filter = "none")}
+          />
+        </Link>
+
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           Running Start Digital
         </Typography>
