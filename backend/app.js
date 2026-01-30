bb8186dbb4efd026422e6340a50d64c6534f6e1d
alstondsouza1 <alstondsouza35@gmail.com>
2026-01-29 18:44:37 -0800

    checked routing and page structure, connected backend router and basic middleware

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,16 @@
 import express from 'express';
+import router from "./router/router.js";
 
-const PORT = 3000;
 const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use("/api", router);
 
 app.get('/', (req, res) => {
     res.send("Hello")
 });
 
 app.listen(PORT, () => {
-    console.log(`Listening on http://localhost:${PORT}`)
-})
+    console.log(`Listening on http://localhost:${PORT}`);
+});
