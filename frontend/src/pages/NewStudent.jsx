ec4e7fbed6de415c6d6f54c899a012e184aad03b
LAURA <Villaraza.laura@student.greenriver.edu>
2026-01-30 00:07:27 -0800

    added navbar and Footer layout components
    * Installed node to run app
    
    * Made NavBar reuseable own component
    
    * Added footer content into the main app.jsx file
    
    * Added footer content

--- frontend/src/pages/NewStudent.jsx
+++ frontend/src/pages/NewStudent.jsx
@@ -1,9 +1,14 @@
+import Navbar from "../components/Navbar";
+
 export default function NewStudent() {
     return (
-      <div style={{ padding: 24 }}>
-        <h2>New Student</h2>
-        <p>Content coming soon.</p>
-      </div>
+      <>
+        <Navbar />
+        <div style={{ padding: 24 }}>
+          <h2>New Student</h2>
+          <p>Content coming soon. TESTING</p>
+        </div>
+      </>
     );
 }
   
