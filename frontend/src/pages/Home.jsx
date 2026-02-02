0333ef4d70c148fd6114117c8c4df8f45fde43e6
LauraV-702 <Villaraza.Laura@student.greenriver.edu>
2026-02-02 13:44:46 -0800

    Testing branch

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
       <div style={{ padding: 24 }}>
         <h1>Running Start Digital Support Portal</h1>
-        <p>Project setup in progress.</p>
+        <p>Project setup in progress. TESTING</p>
       </div>
     );
 }
