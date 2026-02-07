9216f09c0199670b7e4af712073b0ca3a3d54051
alstondsouza1 <alstondsouza35@gmail.com>
2026-02-07 04:21:56 -0800

    enabled/added cors in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import router from "./router/router.js";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/api", router);
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello");
