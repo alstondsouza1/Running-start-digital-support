50811eb4d2eeb46087b3e816a060fd137749e3be
alstondsouza1 <alstondsouza35@gmail.com>
2026-02-11 21:45:12 -0800

    added current student FAQ data file

---
+++ frontend/src/data/currentStydent.js
@@ -0,0 +1,27 @@
+export const currentStudentsQuestions = [
+    {
+      type: "dates-deadlines",
+      question: "What happens if I miss the enrollment deadline?",
+      answer: {
+        bullets: [
+          {
+            text:
+              "You will not be able to participate in Running Start for that term. You must be enrolled by the third day of the term.",
+          },
+        ],
+      },
+    },
+    {
+      type: "dates-deadlines",
+      question: "What if my records are blocked when I try to enroll in classes?",
+      answer: {
+        intro: "Ask yourself these questions:",
+        bullets: [
+          { text: "Have I submitted my Enrollment Verification form for this term?" },
+          { text: "Did I meet Academic Standards last term (GPA 2.0+ required)?" },
+          { text: "Do I have any outstanding fines or parking tickets in ctcLink?" },
+        ],
+      },
+    },
+  ];
+  
