7541a1e15f919efb6186e86075f2e0f2c4c653aa
Danny McCarragher <dannymccarragher@gmail.com>
2026-03-01 20:49:27 -0800

    update process.env

--- backend/db/db.js
+++ backend/db/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    database: process.env.DB_DATABASE,
+    database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD
 });
 
